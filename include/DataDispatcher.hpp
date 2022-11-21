d7c17cef327a60dd077301075248dc411abd38d7
Robot-WH <3062451557@qq.com>
2022-11-21 18:23:26 +0800

    完善线程同步,mutex改为shared_mutex

--- include/DataDispatcher.hpp
+++ include/DataDispatcher.hpp
@@ -5,10 +5,12 @@
 #include <deque>
 #include <functional>
 #include <mutex>
+#include <shared_mutex>
 #include <thread>
 #include <condition_variable>
 #include <typeindex>
 #include <type_traits>
+#include <atomic>
 #include "RemoveReference.hpp"
 #include "Function.hpp"
 
@@ -28,6 +30,7 @@ class DataContainerBase {
         virtual inline uint16_t GetDataSize() const = 0; 
         virtual inline void DeleteFrontData() = 0; 
         virtual bool Callback() = 0;  
+        virtual inline bool IsEmpty() const = 0; 
         inline uint16_t GetCapacity() const {
             return capacity_;  
         }
@@ -44,7 +47,9 @@ template<typename _DataT>
 class DataContainerImpl : public DataContainerBase {
     public:
         DataContainerImpl(uint16_t const& capacity, FunctionBase* p_callback_wrapper) 
-        : DataContainerBase(capacity), p_callback_wrapper_(p_callback_wrapper), type_info_(typeid(_DataT)) {}    // typeid 不区分const和&  也就是 const int& 和 int 是一样的
+        : DataContainerBase(capacity), 
+        p_callback_wrapper_(p_callback_wrapper), 
+        type_info_(typeid(_DataT)) {}    // typeid 不区分const和&  也就是 const int& 和 int 是一样的
         
         ~DataContainerImpl() {
             // std::cout << "~DataContainerImpl()" <<std::endl;
@@ -53,19 +58,15 @@ class DataContainerImpl : public DataContainerBase {
 
         /**
          * @brief: 执行回调函数
-         * @return 若缓存中没有数据了 那么不执行回调  返回false, 否则返回true
+         * @return 执行回调是否成功
          */        
         virtual bool Callback() override {
             if (data_buffer_.empty()) return false;   
-            call(data_buffer_.front());
+            call(data_buffer_.front());   // 传入左值   
             DeleteFrontData();  
             return true;  
         }
 
-        // 获取数据类型  
-        inline std::type_index GetDataType() const override {
-            return type_info_;  
-        }
         /**
          * @brief: 在队列最末尾添加数据 
          */             
@@ -97,8 +98,7 @@ class DataContainerImpl : public DataContainerBase {
         // }
 
         /**
-         * @brief: 删除数据
-         * @param[in] index  位于队列上的序号
+         * @brief: 删除头数据
          */            
         inline void DeleteFrontData() override {
             data_m_.lock();
@@ -109,17 +109,26 @@ class DataContainerImpl : public DataContainerBase {
         virtual inline uint16_t GetDataSize() const override {
             return data_buffer_.size();  
         }
-    private:
 
+        // 获取数据类型  
+        inline std::type_index GetDataType() const override {
+            return type_info_;  
+        }
+        
+        virtual inline bool IsEmpty() const override {
+            return data_buffer_.size() == 0;   
+        }
+
+    private:
         /**
          * @brief: 触发回调的接口
-         * @param data 右值引用  
+         * @param data 万能引用  
          */        
         template<typename _T>
         void call(_T&& data) {
             Function<void(remove_reference_t<_DataT>&)>* p_callback =  
-                reinterpret_cast<Function<void(remove_reference_t<_DataT>&)>*>(p_callback_wrapper_);
-            (*p_callback)(data);   // 将数据传送到回调函数
+                dynamic_cast<Function<void(remove_reference_t<_DataT>&)>*>(p_callback_wrapper_);
+            (*p_callback)(data);   // 由于 p_callback 绑定的参数为左值引用，所以不需要对data进行完美转发。输入为右值时，data也是个左值
         }
 
         std::mutex data_m_;   // 加了mutex后变成了只移型别   (禁止了拷贝构造)
@@ -184,8 +193,18 @@ class Subscriber {
         // }
 
     private:
+        /**
+         * @brief: 回调
+         */        
         bool callback() {
             return data_cache_->Callback();  
+        }
+
+        /**
+         * @brief: 检查是否所有缓存数据都处理完毕
+         */        
+        bool cacheDataEmpty() {
+            return data_cache_->IsEmpty();  
         }
 
         template<typename _DataT>
@@ -198,7 +217,7 @@ class Subscriber {
             using pure_type = typename std::remove_const<remove_reference_t<_DataT>>::type; 
             DataContainerImpl<pure_type>* data_container_ptr =  
                 dynamic_cast<DataContainerImpl<pure_type>*>(data_cache_);
-            data_container_ptr->AddData(std::forward<_DataT>(data));  
+            data_container_ptr->AddData(std::forward<_DataT>(data));     // 线程安全的
         }
 
         friend class DataDispatcher; 
@@ -242,7 +261,10 @@ class DataDispatcher {
                                                     int cache_capacity = 1) {
             Subscriber* p_subscriber = new Subscriber(callback, class_addr, cache_capacity);
             substriber_container_m_.lock();
+            active_data_container_m_.lock();  
+            active_data_container_[name] = false;  
             subscriber_container_[name].insert(p_subscriber); 
+            active_data_container_m_.unlock();  
             substriber_container_m_.unlock();  
             return *p_subscriber;  
         }
@@ -259,7 +281,10 @@ class DataDispatcher {
                                                         int cache_capacity = 1) {
             Subscriber* p_subscriber = new Subscriber(callback, cache_capacity);
             substriber_container_m_.lock();
+            active_data_container_m_.lock();  
+            active_data_container_[name] = false;  
             subscriber_container_[name].insert(p_subscriber); 
+            active_data_container_m_.unlock();  
             substriber_container_m_.unlock();  
             return *p_subscriber;  
         }
@@ -273,15 +298,24 @@ class DataDispatcher {
         template<typename _T>
         void Publish(std::string const& name, _T&& data) {
             // 如果有订阅者  则将数据传送到各个订阅者的数据缓存区
-            // std::cout << "topic name: " << name << ", subscriber size: " 
-            // << subscriber_container_[name].size() << std::endl;
+            std::shared_lock<std::shared_mutex> m_l(substriber_container_m_);  // 禁止subscriber_container_ 写数据
             if (subscriber_container_[name].size()) {
+                ///////////////////////////////////////////////////
+                data_m_.lock();  
                 for (const auto& pt : subscriber_container_[name]) {
                     pt->addData(std::forward<_T>(data));     // addData是线程安全的
                 }
-                active_data_container_m_.lock();
-                active_data_container_[name] = true;  
-                active_data_container_m_.unlock();  
+                data_m_.unlock();
+                ///////////////////////////////////////////////////
+                m_l.unlock();     // = unlock_shared(), 可以对 subscriber_container_ 进行更新了
+                ///////////////////////////////////////////////////
+                active_data_container_m_.lock_shared();
+                if (!active_data_container_.at(name)) {
+                    active_data_container_.at(name) = true;    // 将name 设置为活跃 ， 表示有新数据 
+                    ++active_num;
+                }
+                active_data_container_m_.unlock_shared();  
+                ///////////////////////////////////////////////////
                 // std::cout << "active_data_container_ name: " << name << "=true" << std::endl;
                 con_.notify_one();  
             }
@@ -298,34 +332,46 @@ class DataDispatcher {
         // 处理线程   
         void process() {
             while (true) {
-                std::unique_lock<std::mutex> lock_m(data_m_);
+                std::unique_lock<std::mutex> lock_m(process_m_);
                 con_.wait(lock_m,[&] {
-                    return active_data_container_.size() > 0;
+                    return active_num > 0;
                 });
-                // 遍历每一个有新数据的容器
+                // 遍历每一个topic，并找到活跃的(接受到了新数据)
+                std::shared_lock<std::shared_mutex> s_l(active_data_container_m_); // 禁止写，防止active_data_container_遍历失败
                 for (const auto& obj : active_data_container_) {
-                    // 遍历该容器的全部订阅者
-                    int count = 0; 
+                    if (!obj.second) continue;   // 如果没有新数据 则跳过 
+                    // 该容器的全部订阅者都执行回调函数
                     for (const auto& pt : subscriber_container_[obj.first]) {
-                        if (!pt->callback()) {
-                            ++count;  
-                        } 
+                        pt->callback();   // callback() 是线程安全的
                     }
-                    // 如果订阅者全部数据都回调完毕  则失能
-                    if (count == subscriber_container_[obj.first].size()) {
-                        active_data_container_.erase(obj.first);
+                    // 检查该数据容器的全部订阅者是否都处理完了自己的数据
+                    data_m_.lock(); 
+                    int finish = true; 
+                    for (const auto& pt : subscriber_container_[obj.first]) {
+                        if (!pt->cacheDataEmpty()) {
+                            finish = false;
+                            break;
+                        }
+                    }
+                    // 如果所有订阅者的数据都回调完毕  则失能
+                    if (finish) {
+                        active_data_container_.at(obj.first) = false;  
+                        --active_num; 
                         // std::cout << "active_data_container_ erase: " << obj.first <<std::endl;
                         // std::cout << "----------------------------------------------" <<std::endl;
                     }
+                    data_m_.unlock();  
                 }
             };
         }
 
     private:
         std::mutex data_m_; 
+        std::mutex process_m_; 
         std::mutex data_container_m_; 
-        std::mutex substriber_container_m_; 
-        std::mutex active_data_container_m_;  
+        std::shared_mutex substriber_container_m_; 
+        std::shared_mutex active_data_container_m_;  
+        std::atomic<int> active_num{0}; 
         std::condition_variable con_;   
         std::thread callback_thread_;   
         std::unordered_map<std::string, std::set<Subscriber*>> subscriber_container_;    // 保存订阅话题 name 的全部 订阅者 
